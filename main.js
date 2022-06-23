d47ae14ad92e35acd3e6b33e487da564f99e8cd3
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-06-23 19:12:26 +0300

    Update main.js
    I couldn´t do the response not ok for the input 42. I´ll keep trying. This is without the callback from the server for the input 42

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const url ='http://localhost:5050/fibonacci/'
 const input = document.getElementById('input');
-
+const form = document.querySelector('.form-control');
 
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
+function onSpinner(){
+  let spinnerOn = document.getElementById('result');
+  spinnerOn.classList.remove('d-none');
+}
+function offSpinner(){
+  const spinnerOff = document.getElementById('result');
+  spinnerOff.classList.remove('spinner-border');
+}
 
 btn.addEventListener('click',function(){
-  const urlComplete = url + input.value;
-
+  onSpinner();
+  let urlComplete = url + input.value;
+  
+  if(input.value<=50 && input.value>=0){
   fetch(urlComplete)
   .then((response) => response.json())
-  .then(function(data){ 
+    .then(function(data){ 
+      offSpinner();
     console.log(data.result);
     result.innerHTML=data.result;
+    })}
+     else if(input.value>50){
+      offSpinner();
+      form.classList.add('text-danger');
+      result.innerHTML="Can't be larger than 50";
+      result.classList.add('text-danger');
+      
+        }
+    else if(input.value<0){
+      offSpinner();
+      form.classList.add('text-danger');
+      result.innerHTML="Can't be a negative number";
+      result.classList.add('text-danger');
+     }
+     
+    }
+    )
     
-  }
-    );
-})
-
